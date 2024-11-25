de28f3fe2463dd84e5da6a81d9db581026a532eb
Olga Morozova <olga.ovsejchik@gmail.com>
2024-11-25 14:38:51 +0100

    index.js modified

--- index.js
+++ index.js
@@ -1,60 +1,53 @@
 const http = require('http');
+const console = require('./util/Logger.js').console;
+const { articlesController } = require('./controllers/articlesController.js');
+const { commentsController } = require('./controllers/commentsController.js');
+const { parseBodyJson, parseUrl } = require('./helper.js');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const handlers = {
-    '/sum': sum
+const endpointMapper = {
+    '/sum': sum,
+    ...articlesController,
+    ...commentsController
 };
 
 const server = http.createServer((req, res) => {
-    parseBodyJson(req, (err, payload) => {
-    const handler = getHandler(req.url);
+        const {url} = parseUrl(req.url);
+        const handler = getHandler(url);
 
-    handler(req, res, payload, (err, result) => {
-        if (err) {
-            res.statusCode = err.code;
+        handler(req, res, (err, result) => {
+            if (err) {
+                res.statusCode = 404;
+                res.setHeader('Content-Type', 'application/json');
+                res.end(err.message);
+
+                return;
+            }
+
+            res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
-            res.end( JSON.stringify(err) );
-
-            return;
-        }
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end( JSON.stringify(result) );
-    });
-    });
-});
-
+            res.end(JSON.stringify(result));
+        });
+    }
+);
+     
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 function getHandler(url) {
-    return handlers[url] || notFound;
+    return endpointMapper[url] || notFound;
 }
 
-function sum(req, res, payload, cb) {
-    const result = { c: payload.a + payload.b };
-
-    cb(null, result);
-}
-
-function notFound(req, res, payload, cb) {
-    cb({ code: 404, message: 'Not found'});
-}
-
-function parseBodyJson(req, cb) {
-    let body = [];
-
-    req.on('data', function(chunk) {
-        body.push(chunk);
-    }).on('end', function() {
-        body = Buffer.concat(body).toString();
-
-        let params = JSON .parse(body);
-
-        cb(null, params);
+function sum(req, res, cb) {
+    parseBodyJson(req, (err, payload) => {
+        const result = { c: payload.a + payload.b };
+        cb(null, result); 
     });
+}
+
+function notFound(req, res, cb) {
+    cb({ code: 404, message: 'Not found'});
 }
